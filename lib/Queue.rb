66f8c623d288f0c38a080834ca5360d4e312b631
add2point71dots <alcland@gmail.com>
2017-02-23 22:20:41 -0800

    Defined and tested the clases Stack and Queue

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,22 +1,31 @@
 class Queue
   def initialize
+    @store = Array.new
   end
 
   def enqueue(element)
+    @store.push(element)
   end
 
   def dequeue
+    raise ArgumentError.new("You cannot dequeue an empty Queue") if empty?
+    @store.shift
   end
 
   def front
+    raise ArgumentError.new("You can't call front on an empty Queue") if empty?
+    @store.first
   end
 
   def size
+    @store.length
   end
 
   def empty?
+    @store.empty?
   end
 
   def to_s
+    return @store.to_s
   end
 end
--- lib/Stack.rb
+++ lib/Stack.rb
@@ -4,18 +4,25 @@ class Stack
   end
 
   def push(element)
+    @store.push(element)
   end
 
   def pop
+    raise ArgumentError.new("You cannot pop an empty Stack") if empty?
+    @store.pop
   end
 
   def top
+    raise ArgumentError.new("You cannot call top on an empty Stack") if empty?
+    @store.last
   end
 
   def size
+    @store.length
   end
 
   def empty?
+    @store.empty?
   end
 
   def to_s
